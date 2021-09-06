cca93dedc0e30bc1036a3a5f8a31527905bc301f
Sanjana <89903702+Sanjana86@users.noreply.github.com>
2021-09-06 20:34:06 +0530

    Update optional.sql

--- lab1/optional.sql
+++ lab1/optional.sql
@@ -1,7 +1,7 @@
 use university_profile;
 
 #11.Class teacher wants to find the student name and rollno with the maximum avg in all subjects. ( google sum or avg)
-	select name,rollno,max( ( student.math+student.sci+student.eng+student.social+student.sports) /5) as avgs from student group by name,rollno;
+	select name,rollno from student where ((math+sci+eng+social+sports)/5)=(select max((math+sci+eng+social+sports)/5) from student);
 
 #12.An inquisitive student wants to find out the names of students whose names have the letter ‘a’ in the second position of their name
 	select name from student where INSTR(name,'a')=2;
